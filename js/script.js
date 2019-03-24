9696c1c7a633d168e36bf73b7c07a77caade0ca5
foxisrael <dmitriy.zamanskiy@gmail.com>
2019-03-24 15:18:54 +0200

    JS

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
-    var link = document.querySelector(".header__top__checkout");
-    var popup = document.querySelector(".contacts__popup");
-    var close = document.querySelector(".popup__close");
-    var linkmap = document.querySelector(".about__contacts__map");
-    var map = document.querySelector(".map__popup");
-    var buy = document.querySelector(".button__buy");
+var link = document.querySelector(".header__top__checkout");
+var popup = document.querySelector(".contacts__popup");
+var close = document.querySelector(".button__close");
+var linkmap = document.querySelector(".about__contacts__map");
+var map = document.querySelector(".map__popup");
+var buy = document.querySelectorAll(".button__buy");
+var product = document.querySelector(".product__popup");
 
-    link.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      popup.classList.add("contacts__popup--show");
-    });
+buy.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  product.classList.add("product__popup--show");
+});
 
-    linkmap.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      popup.classList.add("map__popup--show");
-    });
+// close.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   product.classList.remove("product__popup--show");
+//   console.log("Клик по ссылке выход");
+// });
 
-    buy.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      popup.classList.add("product__popup--show");
-    });
-
-    close.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      popup.classList.remove("contacts__popup--show");
-      popup.classList.remove("map__popup--show");
-    });
-
-    window.addEventListener("keydown", function(event) {
-      if (event.keyCode === 27) {
-        popup.classList.remove("contacts__popup--show");
-      }
-    });
+// link.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   popup.classList.add("contacts__popup--show");
+//   console.log("Клик по ссылке вход");
+// });
+//
+// close.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   popup.classList.remove("contacts__popup--show");
+//   console.log("Клик по ссылке выход");
+// });
+//
+// window.addEventListener("keydown", function(event) {
+//   if (event.keyCode === 27) {
+//     popup.classList.remove("contacts__popup--show");
+//   }
+//   console.log("Нажатие кнопки ESC");
+// });
+//
+// linkmap.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   map.classList.add("map__popup--show");
+// });
