47ae6770f4c73691dca3fa6631a74b6647baf87a
foxisrael <dmitriy.zamanskiy@gmail.com>
2019-03-24 15:59:51 +0200

    preFINAL

--- js/script.js
+++ js/script.js
@@ -6,37 +6,21 @@ var map = document.querySelector(".map__popup");
 var buy = document.querySelectorAll(".button__buy");
 var product = document.querySelector(".product__popup");
 
-buy.addEventListener("click", function(evt) {
+link.addEventListener("click", function(evt) {
   evt.preventDefault();
-  product.classList.add("product__popup--show");
+  popup.classList.add("contacts__popup--show");
+  console.log("Клик по ссылке вход");
 });
 
-// close.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   product.classList.remove("product__popup--show");
-//   console.log("Клик по ссылке выход");
-// });
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("contacts__popup--show");
+  console.log("Клик по ссылке выход");
+});
 
-// link.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   popup.classList.add("contacts__popup--show");
-//   console.log("Клик по ссылке вход");
-// });
-//
-// close.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   popup.classList.remove("contacts__popup--show");
-//   console.log("Клик по ссылке выход");
-// });
-//
-// window.addEventListener("keydown", function(event) {
-//   if (event.keyCode === 27) {
-//     popup.classList.remove("contacts__popup--show");
-//   }
-//   console.log("Нажатие кнопки ESC");
-// });
-//
-// linkmap.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   map.classList.add("map__popup--show");
-// });
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    popup.classList.remove("contacts__popup--show");
+  }
+  console.log("Нажатие кнопки ESC");
+});
